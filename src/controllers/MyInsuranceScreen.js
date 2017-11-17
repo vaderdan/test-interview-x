8ee9fd96e1d87dce1ac69a931c9de943f63d7c7e
Danny Lazarow <danny.lazarow@gmail.com>
2017-11-17 23:02:02 +0200

    show sum of all insurances

--- src/controllers/MyInsuranceScreen.js
+++ src/controllers/MyInsuranceScreen.js
@@ -1,6 +1,10 @@
 import _ from 'lodash'
 import globalVariables from '../config/styles.config'
 import { width, height } from '../config/dimensions.config'
+import accounting from 'accounting'
+import {observer} from 'mobx-react/native'
+
+import InsuranceService from '../services/InsuranceService'
 
 const React = require('react')
 const {
@@ -11,13 +15,16 @@ const {
     TouchableHighlight
 } = require('react-native')
 
-import Icon from 'react-native-vector-icons/FontAwesome'
+import Pagination from '../lib/Pagination'
 
-class MyInsuranceScreen extends React.Component {
+@observer class MyInsuranceScreen extends React.Component {
 
     render() {
+        //this hack is needed so to observe insurance list
+        this.props.insurancePagination.results.length
+
         return <View style={styles.containerMain}>
-            <Text style={styles.mainTitle}>This Month you save <Text style={styles.mainTitleBold}>$98.00</Text></Text>
+            <Text style={styles.mainTitle}> Your current balance <Text style={styles.mainTitleBold}>{accounting.formatMoney(_.toNumber(InsuranceService.sumInsurances()), 'CHF ', 0)}</Text></Text>
             <Image style={styles.mainImage} resizeMode="contain" source={require('../images/front_image.png')}/>
         </View>
     }
